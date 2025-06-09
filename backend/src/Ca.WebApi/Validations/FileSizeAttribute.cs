bb6dd01d7df62c313f63b9fb5f5cda700f853dd0
Reza Taba <mrtabaa@gmail.com>
2025-06-09 14:55:32 +0300

    Added ValidationAttributes

---
+++ backend/src/Ca.WebApi/Validations/FileSizeAttribute.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Ca.WebApi.Validations;
+
+/// <summary>
+///     Set a minimum and maximum size for input files.
+/// </summary>
+/// <param name="minFileSize"></param>
+/// <param name="maxFileSize"></param>
+public class FileSizeAttribute(long minFileSize, long maxFileSize) : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? values, ValidationContext validationContext)
+    {
+        if (values is not IFormFile file) return ValidationResult.Success;
+
+        if (file.Length < minFileSize) return new ValidationResult($"One or more of the files is below the allowed minimum size of {minFileSize / 1_000} kilobytes.");
+
+        if (file.Length > maxFileSize) return new ValidationResult($"One or more of the files is over the allowed maximum size of {maxFileSize / 1_000_000} megabytes.");
+
+        return ValidationResult.Success;
+    }
+}
