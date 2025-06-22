e3e3bff465f6b45006aca091be494af96c05e194
Reza Taba <mrtabaa@gmail.com>
2025-06-22 19:28:37 +0400

    Refactored seeding to extension

---
+++ backend/src/Ca.WebApi/Extensions/SeedWebApplicationExtensions.cs
@@ -0,0 +1,76 @@
+using Ca.Application.Modules.AccessControl;
+using Ca.Application.Modules.AccessControl.Commands;
+using Ca.Application.Modules.Auth;
+using Ca.Application.Modules.Auth.Commands;
+using Ca.Contracts.Responses.AccessControl;
+using Ca.Contracts.Responses.Auth;
+using Ca.Domain.Modules.AccessControl.Enums;
+using Ca.Shared.Configurations.Common.SeedSettings;
+using Ca.Shared.Results;
+using Ca.WebApi.Modules.Auth;
+using Microsoft.Extensions.Options;
+
+namespace Ca.WebApi.Extensions;
+
+internal static class SeedWebApplicationExtensions
+{
+    internal static async Task UseAppSeeder(this WebApplication app)
+    {
+        using IServiceScope scope = app.Services.CreateScope();
+        SuperAdminSeedInfo superAdminSeedInfo = app.Services.GetRequiredService<IOptions<SuperAdminSeedInfo>>().Value;
+        
+        await CreateSuperAdminRole(scope, superAdminSeedInfo);
+        await CreateSuperAdmin(scope, superAdminSeedInfo);
+        await CreateClientRole(scope);
+    }
+
+    private static async Task CreateSuperAdminRole(IServiceScope scope, SuperAdminSeedInfo seedInfo)
+    {
+        var accessControlService = scope.ServiceProvider.GetRequiredService<IAccessControlService>();
+
+        var command = new AccessRoleCommand(
+            seedInfo.RoleName, [AccessPermissionType.AccessSuperAdminPanel.ToString()]
+        );
+
+        OperationResult<AccessRoleResponse> result =
+            await accessControlService.SeedSuperAdminRoleAndPermissionsAsync(command);
+        Console.WriteLine(
+            result.IsSuccess
+                ? $"{command.RoleName} role created successfully."
+                : $"{command.RoleName} role creation failed with error {result.Error?.Message}."
+        );
+    }
+
+    private static async Task CreateSuperAdmin(IServiceScope scope, SuperAdminSeedInfo seedInfo)
+    {
+        var authService = scope.ServiceProvider.GetRequiredService<IAuthService>();
+
+        RegisterSuperAdminCommand command = AuthRequestMapper.MapRegisterSuperAdminRequestToRegisterCommand(seedInfo);
+
+        OperationResult<RegisterResponse> registerResult = await authService.SeedSuperAdminAppUserAsync(command);
+        Console.WriteLine(
+            registerResult.IsSuccess
+                ? $"{command.UserName} created successfully."
+                : $"{command.UserName} creation failed with error {registerResult.Error?.Message}."
+        );
+    }
+
+    private static async Task CreateClientRole(IServiceScope scope)
+    {
+        var accessControlService = scope.ServiceProvider.GetRequiredService<IAccessControlService>();
+
+        var command = new AccessRoleCommand(
+            "Client", [
+                AccessPermissionType.EditOwnProfile.ToString(),
+                AccessPermissionType.SendMessages.ToString()
+            ]
+        );
+
+        OperationResult<AccessRoleResponse> result = await accessControlService.UpsertRoleAsync(command);
+        Console.WriteLine(
+            result.IsSuccess
+                ? $"{command.RoleName} role created successfully."
+                : $"{command.RoleName} role creation failed with error {result.Error?.Message}."
+        );
+    }
+}
--- backend/src/Ca.WebApi/Program.cs
+++ backend/src/Ca.WebApi/Program.cs
@@ -1,15 +1,5 @@
-using Ca.Application.Modules.AccessControl;
-using Ca.Application.Modules.AccessControl.Commands;
-using Ca.Application.Modules.Auth;
-using Ca.Application.Modules.Auth.Commands;
-using Ca.Contracts.Responses.AccessControl;
-using Ca.Contracts.Responses.Auth;
-using Ca.Domain.Modules.AccessControl.Enums;
-using Ca.Shared.Configurations.Common.SeedSettings;
-using Ca.Shared.Results;
 using Ca.WebApi;
-using Ca.WebApi.Modules.Auth;
-using Microsoft.Extensions.Options;
+using Ca.WebApi.Extensions;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -31,28 +21,9 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.UseSwaggerUI(options => { options.SwaggerEndpoint("/openapi/v1.json", "Ca.WebApi"); }
     );
-
-    // Seed SuperAdmin
-    SuperAdminSeedInfo seedInfo = app.Services.GetRequiredService<IOptions<SuperAdminSeedInfo>>().Value;
-
-    RegisterSuperAdminCommand command = AuthRequestMapper.MapRegisterSuperAdminRequestToRegisterCommand(seedInfo);
-
-    using IServiceScope scope = app.Services.CreateScope();
-
-    var accessControlService = scope.ServiceProvider.GetRequiredService<IAccessControlService>();
-    var myCommand = new AccessRoleCommand(seedInfo.RoleName, [AccessPermissionType.AccessSuperAdminPanel.ToString()]);
-    OperationResult<AccessRoleResponse> result =
-        await accessControlService.SeedSuperAdminRoleAndPermissionsAsync(myCommand);
-    Console.WriteLine(result.IsSuccess ? "SuperAdmin role created successfully." : "SuperAdmin role creation failed.");
-
-    var authService = scope.ServiceProvider.GetRequiredService<IAuthService>();
-    OperationResult<RegisterResponse> response = await authService.SeedSuperAdminAppUserAsync(command);
-    Console.WriteLine(
-        response.IsSuccess
-            ? "SuperAdmin created successfully."
-            : $"SuperAdmin creation failed with error {response.Error?.Message}."
-    );
 }
+
+await app.UseAppSeeder();
 
 app.UseHttpsRedirection();
 
