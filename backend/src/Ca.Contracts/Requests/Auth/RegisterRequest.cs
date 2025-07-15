41ea437661012cf8f011f41035e47dc8bffbe15d
Reza Taba <mrtabaa@gmail.com>
2025-07-15 15:50:05 +0400

    Added prop validations

--- backend/src/Ca.Contracts/Requests/Auth/RegisterRequest.cs
+++ backend/src/Ca.Contracts/Requests/Auth/RegisterRequest.cs
@@ -1,10 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using Ca.Domain.Modules.Auth.Constants;
+using Ca.Domain.Shared;
+
 namespace Ca.Contracts.Requests.Auth;
 
 public record RegisterRequest(
+    [Length(SharedLengths.NameMin, SharedLengths.NameMax)]
     string FirstName,
+    [Length(SharedLengths.NameMin, SharedLengths.NameMax)]
     string LastName,
-    string Email,
+    [MaxLength(SharedLengths.EmailMax)] string Email,
+    [Length(SharedLengths.NameMin, SharedLengths.NameMax)]
     string UserName,
+    [DataType(DataType.Password)]
+    [Length(AuthLengths.PasswordMin, AuthLengths.PasswordMax)]
+    [RegularExpression(
+        @"^(?=.*[A-Z])(?=.*\d).+$",
+        ErrorMessage = "Needs: 8 to 50 characters. An uppercase character(ABC). A number(123)"
+    )]
     string Password,
     string ConfirmPassword
 );
