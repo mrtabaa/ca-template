eb653fc6cc5bf044bd0ae2c84c027521acb7524d
Reza Taba <mrtabaa@gmail.com>
2025-08-11 16:49:58 +0400

    Added AppUserMongo usings

--- backend/src/Ca.Infrastructure/Modules/Auth/Mongo/AuthMapperMongo.cs
+++ backend/src/Ca.Infrastructure/Modules/Auth/Mongo/AuthMapperMongo.cs
@@ -1,5 +1,16 @@
+using Ca.Domain.Modules.Auth.Aggregates;
+using Ca.Infrastructure.Modules.Auth.Mongo.Models;
+
 namespace Ca.Infrastructure.Modules.Auth.Mongo;
 
 internal static class AuthMapperMongo
 {
+    internal static AppUserMongo MapAppUserToAppUserMongo(AppUser appUser) =>
+        new()
+        {
+            FirstName = appUser.FirstName.Value,
+            LastName = appUser.LastName.Value,
+            Email = appUser.Email.Value,
+            UserName = appUser.UserName.Value
+        };
 }
